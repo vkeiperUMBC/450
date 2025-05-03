e6db18468d08696dc7a09bc4d268ccf30564de89
MattB789 <mbainbr1@umbc.edu>
2025-05-03 19:25:35 -0400

    Fixed Code and added Testing File

---
+++ tb.sv
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+module tb ();
+ logic [13:0] rstring;//the input string
+ logic clk = 1;//the clock
+ logic rst;//the reset
+ logic [2:0] size; //the size of the input string
+ logic [6:0] dstring;//the output string
+ logic done; //signal saying it is done decoding
+decoder decoder(
+    .rstring(rstring),
+    .clk(clk),
+    .rst(rst),
+    .size(size),
+    .dstring(dstring),
+    
+    .done(done)
+    );
+initial begin
+        forever #5ns clk = !clk;
+    end
+
+initial begin
+size = 2'b11;
+rstring = 14'b001101101010;
+$monitor("dstring is %0b", dstring);
+end
+endmodule
