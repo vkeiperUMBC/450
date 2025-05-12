a47bde54053d531af9c274902d26d7d0516eae73
MattB789 <mbainbr1@umbc.edu>
2025-05-12 19:32:23 -0400

    Update tb.sv

--- tb.sv
+++ tb.sv
@@ -3,6 +3,7 @@ module tb ();
  logic [13:0] rstring;//the input string
  logic clk = 1;//the clock
  logic rst;//the reset
+ logic enable; //enable
  logic [2:0] size; //the size of the input string
  logic [6:0] dstring;//the output string
  logic done; //signal saying it is done decoding
@@ -11,6 +12,7 @@ decoder decoder(
     .clk(clk),
     .rst(rst),
     .size(size),
+    .enable(enable),
     .dstring(dstring),
     
     .done(done)
@@ -20,15 +22,9 @@ initial begin
     end
 
 initial begin
+enable = 1'b1;
 size = 2'b11;
 rstring = 14'b11111001101011;
-$monitor("dstring is %0b", dstring);
-@(posedge clk);
-@(posedge clk);
-@(posedge clk);
-@(posedge clk);
-@(posedge clk);
-size = 2'b00;
-rstring = 6'b111111;
+
 end
 endmodule
